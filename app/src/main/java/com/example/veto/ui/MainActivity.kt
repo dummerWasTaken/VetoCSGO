6012712a91956e025872d2d31dc03a86bbdb5057
Rafael <rafaelchmenezes@hotmail.com>
2023-04-05 10:39:24 -0300

    adding changeTextColor and changeImageColor

--- app/src/main/java/com/example/veto/ui/MainActivity.kt
+++ app/src/main/java/com/example/veto/ui/MainActivity.kt
@@ -1,19 +1,15 @@
 /*
-
+FEITO:
 primeiro sempre vai ser BAN
-
-	quando tiver 2 ban, muda pra pick
-
-	quando tiver 2 pick, muda pra ban
-
-	2ban > 2pick > 2ban
-
-	o mapa que sobrar vai ser o "decider"
-
+quando tiver 2 ban, muda pra pick
+quando tiver 2 pick, muda pra ban
+2ban > 2pick > 2ban
 ban -> deixa imagem preto e branco, mostra nome cor vermelha
-
 pick -> mantem a cor da imagem, mostra nome cor verde
-
+----------------------------------------------------------------------------------------------------
+A FAZER:
+ultimo mapa do mapSet tem que ser decider, ou seja, texto tem que mudar pra verde
+nao permitir que a mesma imagem seja clicada mais que uma vez (se for clicada n pode acontecer nada)
  */
 
 package com.example.veto.ui
@@ -37,13 +33,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var bannedMapSet: MutableSet<MapItem> = mutableSetOf()
     private var pickedMapSet: MutableSet<MapItem> = mutableSetOf()
-
     private var vetoStage = VetoConstants.VETOSTAGE.BAN
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -54,12 +49,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
 
         val id: Int = view.id
-
         pickOrBan(id)
-
     }
 
     private fun setUpArrayOfMaps() {
+
         arrayOfMaps = arrayListOf(
             MapItem(R.id.image_inferno, "inferno.jpg", "INFERNO", 0),
             MapItem(R.id.image_mirage, "mirage.jpg", "MIRAGE", 0),
@@ -68,12 +62,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             MapItem(R.id.image_nuke, "nuke.jpg", "NUKE", 0),
             MapItem(R.id.image_ancient, "ancient.jpg", "ANCIENT", 0),
             MapItem(R.id.image_anubis, "anubis.jpg", "ANUBIS", 0)
-
         )
     }
 
-
     private fun setListeners() {
+
         binding.imageAncient.setOnClickListener(this)
         binding.imageAnubis.setOnClickListener(this)
         binding.imageInferno.setOnClickListener(this)
@@ -87,48 +80,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val mapItemIndex = arrayOfMaps.indexOfFirst { it.id == id }
         val mapItem = arrayOfMaps[mapItemIndex]
-
         mapItem.stage = vetoStage
-
         arrayOfMaps[mapItemIndex] = mapItem
-
     }
 
     private fun updateMapSet(id: Int) {
 
         val mapItem = arrayOfMaps.first { it.id == id }
 
+        if (vetoStage == 0) {
+
+        }
         if (vetoStage == 1) {
             bannedMapSet.add(mapItem)
         }
-
         if (vetoStage == 2) {
             pickedMapSet.add(mapItem)
         }
-
     }
 
     private fun checkMapSet() {
-
-        if (bannedMapSet.count() >= 4 && pickedMapSet.count() >= 2) {
-
-        }
 
         if (bannedMapSet.count() >= 2 && pickedMapSet.count() >= 2) {
             vetoStage = VetoConstants.VETOSTAGE.BAN
             return
         }
-
         if (bannedMapSet.count() > 2) {
             vetoStage = VetoConstants.VETOSTAGE.PICK
             return
         }
-
         if (pickedMapSet.count() > 2) {
             vetoStage = VetoConstants.VETOSTAGE.BAN
             return
         }
-
+        if (bannedMapSet.count() >= 4 && pickedMapSet.count() >= 2) {
+            vetoStage = VetoConstants.VETOSTAGE.IDLE
+            return
+        }
     }
 
     private fun pickOrBan(id: Int) {
@@ -138,153 +126,73 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         checkMapSet()
 
         when (vetoStage) {
-
             VetoConstants.VETOSTAGE.PICK -> {
-
                 when (id) {
                     R.id.image_inferno -> {
-
-                        val textView: TextView = findViewById(R.id.text_inferno)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_inferno)
                     }
 
                     R.id.image_anubis -> {
-
-                        val textView = findViewById<TextView>(R.id.text_anubis)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_anubis)
                     }
 
                     R.id.image_mirage -> {
-
-                        val textView: TextView = findViewById(R.id.text_mirage)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_mirage)
                     }
 
                     R.id.image_nuke -> {
-
-                        val textView: TextView = findViewById(R.id.text_nuke)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_nuke)
                     }
 
                     R.id.image_ancient -> {
-
-                        val textView: TextView = findViewById(R.id.text_ancient)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_ancient)
                     }
 
                     R.id.image_overpass -> {
-
-                        val textView: TextView = findViewById(R.id.text_overpass)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_overpass)
                     }
 
                     R.id.image_vertigo -> {
-
-                        val textView: TextView = findViewById(R.id.text_vertigo)
-                        textView.setTextColor(getColor(R.color.green))
+                        changeTextColor(R.id.text_vertigo)
                     }
-
                 }
             }
 
             VetoConstants.VETOSTAGE.BAN -> {
                 when (id) {
                     R.id.image_inferno -> {
-
-                        val imageView: ImageView =
-                            findViewById<View>(R.id.image_inferno) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageView.colorFilter = filter
-
-
-                        val textView: TextView = findViewById(R.id.text_inferno)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_inferno)
                     }
 
                     R.id.image_anubis -> {
-
-                        val imageview: ImageView =
-                            findViewById<View>(R.id.image_anubis) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_anubis)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_anubis)
                     }
 
                     R.id.image_mirage -> {
-
-                        val imageview: ImageView =
-                            findViewById<View>(R.id.image_mirage) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_mirage)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_mirage)
                     }
 
                     R.id.image_nuke -> {
-
-                        val imageview: ImageView = findViewById<View>(R.id.image_nuke) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_nuke)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_nuke)
                     }
 
                     R.id.image_ancient -> {
-
-                        val imageview: ImageView =
-                            findViewById<View>(R.id.image_ancient) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_ancient)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_ancient)
                     }
 
                     R.id.image_overpass -> {
-
-                        val imageview: ImageView =
-                            findViewById<View>(R.id.image_overpass) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_overpass)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_overpass)
                     }
 
                     R.id.image_vertigo -> {
-
-                        val imageview: ImageView =
-                            findViewById<View>(R.id.image_vertigo) as ImageView
-                        val matrix = ColorMatrix()
-                        matrix.setSaturation(0f)
-
-                        val filter = ColorMatrixColorFilter(matrix)
-                        imageview.colorFilter = filter
-
-                        val textView: TextView = findViewById(R.id.text_vertigo)
-                        textView.setTextColor(getColor(R.color.red))
+                        changeImageColor(id)
+                        changeTextColor(R.id.text_vertigo)
                     }
 
                 }
@@ -292,4 +200,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun changeTextColor(id: Int) {
+
+        val textView: TextView = findViewById(id)
+        when (vetoStage) {
+            0 -> {
+                textView.setTextColor(getColor(R.color.green))
+            }
+            1 -> {
+                textView.setTextColor(getColor(R.color.red))
+            }
+            2 -> {
+                textView.setTextColor(getColor(R.color.green))
+            }
+        }
+    }
+
+    private fun changeImageColor(id: Int) {
+
+        val imageView: ImageView = findViewById<View>(id) as ImageView
+        val matrix = ColorMatrix()
+        matrix.setSaturation(0f)
+        val filter = ColorMatrixColorFilter(matrix)
+        imageView.colorFilter = filter
+    }
 }
